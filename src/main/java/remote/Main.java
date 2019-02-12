07e018f4a41fa6f3582925cbc914798c8d529d35
ISE Generic <isegeneric@CRVL203.local>
2019-02-11 23:46:18 -0600

    cleaner main

--- src/main/java/remote/Main.java
+++ src/main/java/remote/Main.java
@@ -1,24 +1,18 @@
 package remote;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
 /**
  * Bose SoundTouch API
  **/
 
 public class Main {
 
-    private static final String FILENAME = "response.xml";
-
     public static void main(String[] args) throws Exception {
+
         BoseSoundTouch boseSoundTouch = new BoseSoundTouch();
 
         if (args != null || args.length > 0) {
             boseSoundTouch.setIP(args[0]);
         }
-
-        Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
         String volume = boseSoundTouch.getVolume();
 
@@ -29,30 +23,5 @@ public class Main {
 
         String now_playing = boseSoundTouch.getNowPlaying();
         System.out.println("Now Playing : " + now_playing);
-    }
-
-    public static void main2(String[] args ) {
-        try {
-//            ApiCall apiCall = new ApiCall();
-//            String endPoint = args[0];
-//
-//            ReadWriteFile readWriteFile = new ReadWriteFile();
-//
-//            String ipAddress = "";
-//            ipAddress = readWriteFile.readFileAsString("IPAddress.txt");
-//
-//            String response = apiCall.getEndPoint(ipAddress, endPoint);
-//
-//            // Store XML response in response.xml file
-//            readWriteFile.usingFileWriter(FILENAME, response);
-//            String xmlResponse = readWriteFile.readFileAsString(FILENAME);
-//
-//            ParseXml parseXml = new ParseXml();
-//            parseXml.parse(xmlResponse);
-//            apiCall.setEndPointValue(ipAddress,endPoint,args[1]);
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 }
---
+++ src/main/java/remote/WasMain.java
@@ -0,0 +1,30 @@
+package remote;
+
+public class WasMain {
+
+//    private static final String FILENAME = "response.xml";
+
+//    public static void main2(String[] args ) {
+//        try {
+//            ApiCall apiCall = new ApiCall();
+//            String endPoint = args[0];
+//
+//            ReadWriteFile readWriteFile = new ReadWriteFile();
+//
+//            String ipAddress = "";
+//            ipAddress = readWriteFile.readFileAsString("IPAddress.txt");
+//
+//            String response = apiCall.getEndPoint(ipAddress, endPoint);
+//
+//            // Store XML response in response.xml file
+//            readWriteFile.usingFileWriter(FILENAME, response);
+//            String xmlResponse = readWriteFile.readFileAsString(FILENAME);
+//
+//            ParseXml parseXml = new ParseXml();
+//            parseXml.parse(xmlResponse);
+//            apiCall.setEndPointValue(ipAddress,endPoint,args[1]);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
+}
